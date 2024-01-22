250b0f2d9dc3449a14b8b7bac55ae24f59175db3
wahyurhy <wahyurahayu82@gmail.com>
2024-01-22 09:15:47 +0700

    laravel Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,7 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
+        <p>By. Wahyu Rahayu in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         {{-- <h5>{{ $post->author }}</h5> --}}
         {!! $post->body !!}
     </article>

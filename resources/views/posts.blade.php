c377b325f5b5cc98897f32b5015ba8ec17b910f8
Wahyu Rahayu <wahyurahayu82@gmail.com>
2024-02-19 09:41:36 +0700

    membuat card

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,13 +3,44 @@
 @section('container')
     <h1 class="mb-5">{{ $title }}</h1>
 
-    @foreach ($posts as $post)
-        <article class="mb-5 border-bottom pb-4">
-            <h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2>
-            <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-            {{-- <h5>By: {{ $post->author }}</h5> --}}
-            <p>{{ $post->excerpt }}</p>
-            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
-        </article>
-    @endforeach
+    <div class="card mb-3">
+        <img src="img/{{ $posts[0]->category->name }}.jpg" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+        <div class="card-body text-center">
+            <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}"
+                    class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
+            <small class="text-muted">
+                <p>By. <a href="/authors/{{ $posts[0]->author->username }}"
+                        class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a
+                        href="/categories/{{ $posts[0]->category->slug }}"
+                        class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+                    {{ $posts[0]->created_at->diffForHumans() }}
+                </p>
+            </small>
+            <p class="card-text">{{ $posts[0]->excerpt }}</p>
+            <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            @foreach ($posts->skip(1) as $post)  
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img class="card-img-top" src="img/{{ $post->category->name }}.jpg" alt="{{ $post->category->name }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $post->title }}</h5>
+                        <small class="text-muted">
+                            <p>By. <a href="/authors/{{ $post->author->username }}"
+                                    class="text-decoration-none">{{ $post->author->name }}</a>
+                                {{ $post->created_at->diffForHumans() }}
+                            </p>
+                        </small>
+                        <p class="card-text">{{ $post->excerpt }}</p>
+                        <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
